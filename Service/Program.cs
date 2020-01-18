20ed8685300d6a0db0480e67a00140ee44747c05
Pavlo Melnyk <pavlomelnyk@ukr.net>
2020-01-18 23:39:35 +0200

    Add project files.

---
+++ Service/Program.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using NLog;
+using NLog.Web;
+
+namespace Service
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+
+            //var logger = NLog.Web.NLogBuilder.ConfigureNLog("nlog.config").GetCurrentClassLogger();
+            //try
+            //{
+            //    logger.Debug("init main");
+            //    CreateHostBuilder(args).Build().Run();
+            //}
+            //catch (Exception exception)
+            //{
+            //    logger.Error(exception, "Stopped program because of exception");
+            //    throw;
+            //}
+            //finally
+            //{
+            //    // Ensure to flush and stop internal timers/threads before application-exit (Avoid segmentation fault on Linux)
+            //    NLog.LogManager.Shutdown();
+            //}
+        }
+
+        //public static IHostBuilder CreateHostBuilder(string[] args) =>
+        //    Host.CreateDefaultBuilder(args)
+        //        .ConfigureServices((hostContext, services) =>
+        //        {
+        //            services.AddHostedService<Worker>();
+        //        }).UseWindowsService();
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureServices((hostContext, services) =>
+                {
+                    services.AddHostedService<Worker>();
+                }).UseNLog().UseWindowsService();
+    }
+}
